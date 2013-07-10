cdb45cd9314c234a7056231939ac4b408ebf1618
Andrew Grieve <agrieve@chromium.org>
2013-07-10 09:31:32 -0400

    [CB-4038] - Remove o.a.c.api namespace

--- src/android/FileTransfer.java
+++ src/android/FileTransfer.java
@@ -53,9 +53,9 @@ import javax.net.ssl.TrustManager;
 import javax.net.ssl.X509TrustManager;
 
 import org.apache.cordova.Config;
-import org.apache.cordova.api.CallbackContext;
-import org.apache.cordova.api.CordovaPlugin;
-import org.apache.cordova.api.PluginResult;
+import org.apache.cordova.CallbackContext;
+import org.apache.cordova.CordovaPlugin;
+import org.apache.cordova.PluginResult;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
