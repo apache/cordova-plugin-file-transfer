2f1411f5dc37854ded35ee31671e66b380b8117f
Ian Clelland <iclelland@chromium.org>
2013-11-22 09:40:56 -0500

    CB-5466: Update FileTransfer plugin to accept filesystem urls

--- src/ios/CDVFileTransfer.h
+++ src/ios/CDVFileTransfer.h
@@ -71,6 +71,7 @@ extern NSString* const kOptionsKeyCookie;
 @property (nonatomic, copy) NSString* objectId;
 @property (nonatomic, copy) NSString* source;
 @property (nonatomic, copy) NSString* target;
+@property (nonatomic, copy) NSURL* targetURL;
 @property (nonatomic, copy) NSString* mimeType;
 @property (assign) int responseCode; // atomic
 @property (nonatomic, assign) long long bytesTransfered;
--- www/FileTransfer.js
+++ www/FileTransfer.js
@@ -184,6 +184,7 @@ FileTransfer.prototype.download = function(source, target, successCallback, erro
             entry.isFile = result.isFile;
             entry.name = result.name;
             entry.fullPath = result.fullPath;
+            entry.filesystem = new FileSystem(result.filesystem == window.PERSISTENT ? 'persistent' : 'temporary');
             successCallback(entry);
         }
     };
